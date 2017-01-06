33c45c385e8ff93c61e6b79cb964d2c6d9b01d99
Ain Tohvri <at@interactive-pioneers.de>
2017-01-06 16:39:18 +0100

    Upgrade dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,9 +93,7 @@ module.exports = function(grunt) {
     },
     jscs: {
       options: {
-        config: '.jscsrc',
-        esnext: false,
-        verbose: true
+        config: '.jscsrc'
       },
       files: {
         src: [
